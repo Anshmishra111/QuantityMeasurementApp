012c8be81ee3d8eb3a8fec974ef89b3e22c44000
himanshu <anshmishra75436@gmail.com>
2026-03-18 11:55:49 +0530

    [Himanshu Mishra] Add. added implementation of feature/UC16-Database-Integration-with-JDBC

--- src/main/java/com/BridgeLab_Training/quantity_measurement_app/util/QuantityMeasurementException.java
+++
@@ -1,8 +0,0 @@
-package com.BridgeLab_Training.quantity_measurement_app.util;
-
-public class QuantityMeasurementException extends RuntimeException {
-
-    public QuantityMeasurementException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/BridgeLab_Training/quantity_measurement_app/util/SupportsArithmetic.java
+++
@@ -1,6 +0,0 @@
-package com.BridgeLab_Training.quantity_measurement_app.util;
-
-@FunctionalInterface
-public interface SupportsArithmetic {
-    boolean isSupported();
-}
